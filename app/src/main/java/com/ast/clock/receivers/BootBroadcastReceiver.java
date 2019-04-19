56ccd33a43bd5061187756a13a71fb08e716a526
Bhagyawant <bhagyawant.biradar@leftrightmind.com>
2019-04-19 20:04:02 +0530

    1. Updated bootreceiver

--- app/src/main/java/com/ast/clock/receivers/BootBroadcastReceiver.java
+++ app/src/main/java/com/ast/clock/receivers/BootBroadcastReceiver.java
@@ -1,12 +1,46 @@
 package com.ast.clock.receivers;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.PowerManager;
+
+import com.ast.clock.utitilies.Constants;
+
+import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (Constants.isForceStopped) {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                String packageName = context.getPackageName();
+                PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+                if (pm.isIgnoringBatteryOptimizations(packageName)) {
+                    setAlarm(context);
+                }
+            }
+        } else {
+            setAlarm(context);
+        }
+    }
 
+
+    private void setAlarm(Context context) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) + 1);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        Intent intentAlarmReciever = new Intent(context, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, Constants.NOTIFY_ID, intentAlarmReciever, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_HOUR, pendingIntent);
+
+        Constants.isForceStopped = false;
     }
 }
